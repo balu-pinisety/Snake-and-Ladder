d9f04a067a548c7ffd4d59bfc8de35f3081243e6
balu-pinisety <manikantapandu225@gmail.com>
2021-06-17 04:12:40 +0530

    Added random function to get Dice Number

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,5 +5,8 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake & Ladder Simulator");
 		//Constant
 		int START_POSITION=0;
+		//Using random to get Dice number 
+		int diceNumber = (int) (Math.floor(Math.random()*10)%6)+1;
+		System.out.println("Dice number is "+diceNumber);
 	}
 }
