c54cb5c577176ee8c938eb0157b5130dc740bd96
balu-pinisety <manikantapandu225@gmail.com>
2021-06-17 05:02:15 +0530

    Added While loop for repeating till the Player reaches the winning position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,24 +5,34 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake & Ladder Simulator");
 		//Constant
 		int START_POSITION=0;
+		int WIN_POSITION=100;
 		int IS_SNAKE=1;
 		int IS_LADDER=2;
+		//Variable
 		int playerPosition=START_POSITION;
-		//Using random to get Dice number 
-		int diceNumber = (int) (Math.floor(Math.random()*10)%6)+1;
-		System.out.println("Dice number is "+diceNumber);
-		//Using random to check for option of No Play, Snake or Ladder
-		int option = (int) Math.floor(Math.random()*10)%3;
-		if (option == IS_LADDER) {
-			System.out.println("Ladder for the Player");
-			playerPosition+=diceNumber;
+		
+		//Repeating till the Player reaches the winning position
+		while(playerPosition<WIN_POSITION){
+			//Using random to get Dice number 
+			int diceNumber = (int) (Math.floor(Math.random()*10)%6)+1;
+			System.out.println("Dice number is "+diceNumber);
+			//Using random to check for option of No Play, Snake or Ladder
+			int option = (int) Math.floor(Math.random()*10)%3;
+			if (option == IS_LADDER) {
+				System.out.println("Ladder for the Player");
+				playerPosition+=diceNumber;
+			} else if (option == IS_SNAKE) {
+				System.out.println("Snake for the Player");
+				playerPosition-=diceNumber;
+				//if player position moves below 0, then the player restarts from Start Position
+				if (playerPosition<0){
+					playerPosition=START_POSITION;
+				}
+			} else {
+				System.out.println("No play for the Player");
+			}
+			//Printing Player Postion
 			System.out.println("Player position is now "+playerPosition);
-		} else if (option == IS_SNAKE) {
-			System.out.println("Snake for the Player");
-			playerPosition-=diceNumber;
-			System.out.println("Player position is now "+playerPosition);
-		} else {
-			System.out.println("No play for the Player");
 		}
 	}
 }
