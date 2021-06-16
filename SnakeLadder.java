f6f960599946e507f553d90cab1f0679fdbc5055
balu-pinisety <manikantapandu225@gmail.com>
2021-06-17 04:01:31 +0530

    Added Constant for Start position as '0'

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,5 +3,7 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		//Displaying Welcome Message
 		System.out.println("Welcome to Snake & Ladder Simulator");
+		//Constant
+		int START_POSITION=0;
 	}
 }
