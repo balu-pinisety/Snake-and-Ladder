25e0f5a73f69f5636d46f232823298292bd36eb8
balu-pinisety <manikantapandu225@gmail.com>
2021-06-17 06:29:49 +0530

    Modified the program for playing of 2 players and choosing the winner

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -10,37 +10,58 @@ public class SnakeLadder {
 		int IS_LADDER=2;
 		//Variable
 		int playerPosition=START_POSITION;
+		int player1Position=START_POSITION;
+		int player2Position=START_POSITION;
 		int diceCount=0;
+		int turn=1;
+		int option=0;
 		
 		//Repeating till the Player reaches the winning position
-		while(playerPosition<WIN_POSITION){
+		while(player1Position<WIN_POSITION && player2Position<WIN_POSITION){
+			//Changing the Player turn
+			if (option != IS_LADDER){
+				++turn;
+			}
+			System.out.println("Player"+(turn%2+1)+"'s turn now");
+			//Assigning the player1 or player2 position to common variable 'playerPosition'
+			if (turn%2+1==1){
+				playerPosition=player1Position;
+			} else {
+				playerPosition=player2Position;
+			}
 			//incremanting the dice count
 			++diceCount;
 			//Using random to get Dice number 
 			int diceNumber = (int) (Math.floor(Math.random()*10)%6)+1;
-			System.out.println("Dice number is "+diceNumber);
+			System.out.println("Dice number is "+diceNumber+" for Player"+(turn%2+1));
 			//Using random to check for option of No Play, Snake or Ladder
-			int option = (int) Math.floor(Math.random()*10)%3;
+			option = (int) Math.floor(Math.random()*10)%3;
 			if (option == IS_LADDER) {
-				System.out.println("Ladder for the Player");
+				System.out.println("Ladder for the Player"+(turn%2+1));
 				playerPosition+=diceNumber;
 				//if player position moves above 100, then the player moves to previous position
 				if (playerPosition>WIN_POSITION){
 					playerPosition-=diceNumber;
 				}
 			} else if (option == IS_SNAKE) {
-				System.out.println("Snake for the Player");
+				System.out.println("Snake for the Player"+(turn%2+1));
 				playerPosition-=diceNumber;
 				//if player position moves below 0, then the player restarts from Start Position
 				if (playerPosition<0){
 					playerPosition=START_POSITION;
 				}
 			} else {
-				System.out.println("No play for the Player");
+				System.out.println("No play for the Player"+(turn%2+1));
 			}
 			//Printing Player Postion
-			System.out.println("Player position is now "+playerPosition);
+			System.out.println("Player"+(turn%2+1)+" position is now "+playerPosition);
+			//Assigning the 'playerPosition' to the individual player
+			if (turn%2+1==1){
+				player1Position=playerPosition;
+			} else {
+				player2Position=playerPosition;
+			}
 		}
-		System.out.println("Dice was rolled "+diceCount+" times to reach the Winning position");
+		System.out.println("\nPlayer"+(turn%2+1)+" is winner\nDice was rolled "+diceCount+" times to reach the Winning position");
 	}
 }
